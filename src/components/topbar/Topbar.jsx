153c13e1e10fd07be580cd52e912f2d4074969c5
Cisco <francisco-hernandez@lambdastudents.com>
2021-07-18 18:16:57 -0700

    added animation to topbar and set up webpage layout

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,32 @@
 import "./topbar.scss"
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
+import PersonIcon from '@material-ui/icons/Person';
 
 export default function Topbar() {
     return (
-        <div className="topbar">
+        <div className="topbar active">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo"> genius.. </a>
+                    <div className="itemcontainer">
+                        <PersonIcon className="icon"/>
+                        <span>+1 208 514 7640</span>
+                    </div>
+                    <div className="itemcontainer">
+                        <AlternateEmailIcon className="icon"/>
+                        <span>francisco.hernandez4412@gmail.com</span>
+                    </div>
+                </div>
+                
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+
+            </div>
             
         </div>
     )
