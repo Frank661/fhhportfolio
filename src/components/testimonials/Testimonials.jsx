8647c948d2a6975958e5fb62bbf6652759b5805c
Frank Hernandez <francisco-hernandez@lambdastudents.com>
2023-04-23 16:52:24 -0700

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,14 +1,12 @@
 import "./testimonials.scss"
 
 export default function Testimonials() {
-
     const data = [
-        
         {
             id: 1,
             name: "Eric Capiz",
-            title: "Freelance Developer",
-            img: "https://media-exp1.licdn.com/dms/image/D5635AQFb83iOUYhELw/profile-framedphoto-shrink_400_400/0/1627609240074?e=1629504000&v=beta&t=8OZL_tkVGfdV3LIOzRqxsQBEshRYUi3pAbRDEnuZLSQ",
+            title: "Front-end Developer at NexTech",
+            img: "https://media-exp1.licdn.com/dms/image/C5603AQHTz8iDf0i0uw/profile-displayphoto-shrink_800_800/0/1633159611387?e=1655337600&v=beta&t=_cDZ8ACD7Q66p2ZfoeQ0Sx7yTlWqpTf2M7PDqk3wxeo",
             icon:"assets/linkedin.png",
             desc: "Frank is a great developer who is easy to work with and always has ideas to better a project, his knowledge with front end developement is 2nd to none. ",
             link: "https://www.linkedin.com/in/eric-capiz/"
@@ -17,7 +15,7 @@ export default function Testimonials() {
             id: 2,
             name: "Alexis Barnett",
             title: "Marketing Manager at Scorpion",
-            img: "https://media-exp1.licdn.com/dms/image/C5603AQENynyEYV5zeA/profile-displayphoto-shrink_200_200/0/1572127264901?e=1632355200&v=beta&t=VGdD-jsIGnrHDalYd1rXrVjEjep0LUdCa2x2Bzfxbxc",
+            img: "https://media-exp1.licdn.com/dms/image/C5603AQENynyEYV5zeA/profile-displayphoto-shrink_100_100/0/1572127264901?e=1655337600&v=beta&t=Ut8Uc0Yq1ba1DMEu8TWWi3vRijHPavRki0xFs8H4BtA",
             icon:"assets/linkedin.png",
             desc: "Frank is an amazing designer who is great at taking feedback and is great at communicating. His quick responsiveness is what makes him a pleasure to work with.  ",
             link: "https://www.linkedin.com/in/barnettab/",
@@ -27,13 +25,12 @@ export default function Testimonials() {
             id: 3,
             name: "Daniel Terry",
             title: "Software Engineer at Atlas RFID Solutions",
-            img: "https://media-exp1.licdn.com/dms/image/C4D03AQHF2DvszFjuuw/profile-displayphoto-shrink_800_800/0/1602443050062?e=1632355200&v=beta&t=QhWjfjL0u6T2rPjiCgiFXIkXpMI4QnFmPxfWI3ott0I",
+            img: "https://media-exp1.licdn.com/dms/image/C4D03AQHF2DvszFjuuw/profile-displayphoto-shrink_100_100/0/1602443050062?e=1655337600&v=beta&t=eOIc0TuYVpT24m8kZx_wKLm0g5iiCpGhDkdsxeVIi5Y",
             icon:"assets/linkedin.png",
             desc: "Frank's awesome to have as a teammate when working on any project small or big, he is quick when it comes to making decisions and is a person who completes his end of the deal on time.",
             link: "https://www.linkedin.com/in/d-terry/"
         }
     ]
-
 
     return (
         <div className= "testimonials" id="testimonials">
